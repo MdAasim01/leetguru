4555d175cfc7a327fd5ac5ee45e81ab30b026892
MdAasim01 <mdaasim.work@gmail.com>
2025-05-27 01:39:50 +0530

    feat: add problem management routes and controller functions

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import problemRoutes from "./routes/problem.routes.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/problems", problemRoutes);
 
 app.listen(process.env.PORT, () => {
 	console.log("Server is running on port 8080");
