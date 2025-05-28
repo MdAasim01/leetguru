26cc79ab775f2789f7c515ae5be43b9e9b1ddba1
MdAasim01 <mdaasim.work@gmail.com>
2025-05-28 23:33:24 +0530

    feat: Add executeCode routes and controllers; update database schema

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import problemRoutes from "./routes/problem.routes.js";
+import executionRoute from "./routes/executeCode.routes.js";
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/problems", problemRoutes);
+app.use("/api/v1/execute-code", executionRoute);
 
 app.listen(process.env.PORT, () => {
 	console.log("Server is running on port 8080");
